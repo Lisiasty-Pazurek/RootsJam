9280eff15f6810df3e828efd234c107cadf11ff3
Lisiasty <69064684+Lisiasty-Pazurek@users.noreply.github.com>
2023-02-03 22:03:13 +0100

    Basic movement & sprites
    Basic movement and iteraction scripts, few sprites

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.UI;
 
 public class Interactable : MonoBehaviour
 {
     bool canInteract;
     bool itemState = false;
+
+    private void OnTriggerStay(Collider other) 
+    {
+        if (other.tag == "Player" && Input.GetKey("F")) 
+        { 
+            Interact();
+        }
+
+        else return;
+        
+    }
 
     public void Interact ()
     {
